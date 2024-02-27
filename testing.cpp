9f1ee6f95b9698d9356093d120f4d4351377f15f
DMackRus <el16dmcr@leeds.ac.uk>
2024-02-27 13:06:05 +0000

    Added extra testing statements, 2 are successful, 1 is unsuccesful. Added a test for ctest in cmake lists.

--- testing.cpp
+++ testing.cpp
@@ -20,5 +20,23 @@ int main(){
         return EXIT_FAILURE;
     }
 
+    int subtraction = subtract(num1, num2);
+    if(subtraction == 5) {
+        std::cout << "Subtraction: " << subtraction << " is correct" << std::endl;
+    }
+    else {
+        std::cout << "Subtraction: " << subtraction << " is incorrect" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    int multiplication = multiply(num1, num2);
+    if(multiplication == 50) {
+        std::cout << "Multiplication: " << multiplication << " is correct" << std::endl;
+    }
+    else {
+        std::cout << "Multiplication: " << multiplication << " is incorrect" << std::endl;
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
